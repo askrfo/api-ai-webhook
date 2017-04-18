72153c83b41a0b12ad4960e4e7e08e9d0dc36637
gomdorya <gomdorya@gmail.com>
2017-04-19 00:38:12 +0900

    webhook

--- index.js
+++ index.js
@@ -13,21 +13,32 @@ restService.post('/hook', function (req, res) {
 
     try {
         var speech = 'empty speech';
+        var nextAction = '';
+        var dataJson = '';
+        var followupEventJson = '';
 
         if (req.body) {
             var requestBody = req.body;
 
             if (requestBody.result) {
                 speech = '';
+                dataJson = '{}';
+                followupEventJson = '{}';
 
-                if (requestBody.result.action === '장치제어'){
-                    speech += '{result:{code:false, message:"장치 제어 입니다."}}';
-                } else if (requestBody.result.action == '배송문의') {
-                    speech += '{result:{code:false, message:"배송 문의 입니다."}}';
-                } else if (requestBody.result.action === 'FAQ') {
-                    speech += '{result:{code:false, message:"FAQ 입니다."}}';
-                } else {
-                    speech += '{result:{code:false, message:"알수 없는 요청 입니다."}}';
+                if (requestBody.result.action) {
+                    if (requestBody.result.action === '장치제어'){
+                        speech += '';
+                    } else if (requestBody.result.action == '인사') {
+                        speech += '안녕하세요';
+                    } else if (requestBody.result.action == '배송문의') {
+                        speech += '배송 문의 입니다.';
+                        dataJson = '{"배송일자":"2017-03-01", "택배사":"대한통운", "도착예상일자":"2017-03-03"}';
+                        followupEventJson = '{"followupEvent":{"name":"shippingTracking","data":{"shippingno":"111111"}}}';
+                    } else if (requestBody.result.action === 'FAQ') {
+                        speech += 'FAQ 입니다.';
+                    } else {
+                        speech += '이해하지 못했습니다.';
+                    }
                 }
 /*
                 if (requestBody.result.fulfillment) {
@@ -48,7 +59,9 @@ restService.post('/hook', function (req, res) {
         return res.json({
             speech: speech,
             displayText: speech,
-            source: 'apiai-webhook-sample'
+            source: 'api-ai-webhook',
+            data: dataJson,
+            followupEvent: followupEventJson
         });
     } catch (err) {
         console.error("Can't process request", err);
