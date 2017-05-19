83543aaf404aa56dc07ca3a0c3dc02b1af50aa48
threebears <gomdorya@gmail.com>
2017-05-19 15:34:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ restService.post('/hook', function (req, res) {
                                     speech: '원하시는 상품을 선택해 주세요',
                                     displayText: '',
                                     source: 'api-ai-webhook',
-                                    data: "{'a':'b'}"
+                                    data: JSON.parse("{'a':'b'}")
                                 });
                                 
                             } else {
